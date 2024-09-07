9edca722c25b35b3ba90c2883f05745bd2a7e1c8
calvin-mcd <114294748+calvin-mcd@users.noreply.github.com>
2024-09-07 18:21:19 +0200

    firmware added

---
+++ Firmware/Source Code/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA0, 0x9C, 0x7D, 0x9A, 0x29, 0x5D, 0xCE, 0x85}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
